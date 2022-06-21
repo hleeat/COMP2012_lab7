47d8d55cb115d7b4c82fe5f5056e0fd0d8cdc951
hleeat <92327104+hleeat@users.noreply.github.com>
2022-06-21 23:15:20 +0900

    Add files via upload

---
+++ Certificate.h
@@ -0,0 +1,77 @@
+#ifndef Certificate_H
+#define Certificate_H
+#include <iostream>
+#include "Issuer.h"
+
+class CertificateBase {
+    protected:
+        int residualTime_;
+        double faceValue_;
+
+    public:
+        bool TestMaturity(int years) {  
+            residualTime_ -= years;
+            if (residualTime_ <= 0) {
+                return true;
+            }
+            return false;
+        }
+
+        double GetFaceValue() const {
+            return faceValue_;
+        }
+
+        virtual void Maturity() = 0;
+};
+
+template<enum BondType bondType>
+class Certificate : public CertificateBase {
+    //TODO: Friend class declaration
+    friend class Issuer<bondType>;
+    private:
+        static int transactionsNum_;
+        static double interestRate_;
+        static int period_;
+
+    private:
+        Issuer<bondType>* issuer_;
+        Certificate(double value, Issuer<bondType>* issuer) {
+        //TODO:
+            //1. Initialized non-static member
+            //2. Increase transactionsNum_ by one 
+            transactionsNum_+=1;
+            faceValue_=value;
+            residualTime_=period_;
+            issuer_=issuer;
+        }
+
+        virtual ~Certificate() {
+        //TODO:
+            //Decrease transactionsNum_ by one 
+            transactionsNum_-=1;
+        }
+
+        static void SetRateAndPeriod(double rate, int year) {
+        //TODO:
+            //Assign argument to the corresponding static member
+            interestRate_=rate;
+            period_=year;
+        }
+
+
+    public:
+        virtual void  Maturity() {
+            issuer_->Settlement(this);
+        }
+};
+
+
+//TODO: Static member initialization, initialize to zero
+template<enum BondType bondType>
+int Certificate<bondType>::transactionsNum_=0;
+template<enum BondType bondType>
+double Certificate<bondType>::interestRate_=0.;
+template<enum BondType bondType>
+int Certificate<bondType>::period_=0;
+
+#endif
